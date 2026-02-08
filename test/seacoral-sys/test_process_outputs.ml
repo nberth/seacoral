361ac4a89b632e591b54c6aead1eb0a3d08b6097
Nicolas Berthier <nicolas.berthier@ocamlpro.com>
2026-02-08 16:40:23 +0100

    Add a test with a retrieval of process output stream that hangs

--- test/seacoral-sys/test_process_outputs.ml
+++ test/seacoral-sys/test_process_outputs.ml
@@ -12,23 +12,42 @@ open Sc_sys.Process.TYPES
 
 open Lwt.Syntax
 
+(* Preliminary utilities *)
+
 let stdout_default = `Grab (Lines (Lwt_fmt.printf "+ %s@."))
 let stderr_default = `Grab (Lines (Lwt_fmt.printf "* %s@."))
 
+let error_default = Lwt_fmt.printf "%a" Sc_sys.Process.pp_unix_status
+
 let sh ?(stdout = stdout_default) ?(stderr = stderr_default) ?timeout
-    ?on_success cmd =
-  Sc_sys.Process.shell_unit cmd ~stdout ~stderr ~log_command:`On_error ?timeout
-    ~on_error:(fun s -> Lwt_fmt.printf "%a" Sc_sys.Process.pp_unix_status s)
-    ?on_success
+    ?on_success ?(on_error = error_default) cmd =
+  Sc_sys.Process.shell_unit cmd ~stdout ~stderr ~log_command:`On_error
+    ?timeout ~on_error ?on_success
 
 let bg ?(stdout = stdout_default) ?(stderr = stderr_default) ?timeout
-    ~on_success cmd =
+    ~on_success ?(on_error = error_default) cmd =
   Sc_sys.Process.shell cmd ~stdout ~stderr ~log_command:`On_error ?timeout
-    ~on_error:(fun s -> Lwt_fmt.printf "%a" Sc_sys.Process.pp_unix_status s)
-    ~on_success
+    ~on_error ~on_success
 
-let%expect_test "delayed-output-retrieval-exit-0" =
+let run ?(timeout = 1.) f =
   Lwt_main.run begin
+    let* () =
+      Lwt.catch begin fun () ->
+        Lwt_unix.with_timeout timeout (fun () -> f)
+      end begin function
+        | Lwt_unix.Timeout ->
+            Lwt_fmt.eprintf "Test timed out"
+        | e ->
+            Lwt.reraise e
+      end
+    in
+    Lwt.join Lwt_fmt.[flush stdout; flush stderr]
+  end
+
+(* Actual tests follow *)
+
+let%expect_test "ok-delayed-output-retrieval-exit-0" =
+  run begin
     let stdout_lines, new_stdout_line = Lwt_stream.create () in
     let* () = sh "echo a" ~stdout:(`Grab (Push_lines new_stdout_line)) in
     Gc.full_major ();         (* <- in case that has an adversarial effect... *)
@@ -37,8 +56,8 @@ let%expect_test "delayed-output-retrieval-exit-0" =
   [%expect{| "a" |}]
 ;;
 
-let%expect_test "delayed-output-retrieval-exit-1" =
-  Lwt_main.run begin
+let%expect_test "ok-delayed-output-retrieval-exit-1" =
+  run begin
     let stdout_lines, new_stdout_line = Lwt_stream.create () in
     let* () = sh "echo a; exit 1" ~stdout:(`Grab (Push_lines new_stdout_line)) in
     Gc.full_major ();         (* <- in case that has an adversarial effect... *)
@@ -47,8 +66,8 @@ let%expect_test "delayed-output-retrieval-exit-1" =
   [%expect{| EXITED(1)"a" |}]
 ;;
 
-let%expect_test "delayed-output-retrieval-delayed-exit-1" =
-  Lwt_main.run begin
+let%expect_test "ok-delayed-output-retrieval-delayed-exit-1" =
+  run begin
     let stdout_lines, new_stdout_line = Lwt_stream.create () in
     let* proc =
       bg "echo a; sleep 10; exit 1"
@@ -62,5 +81,25 @@ let%expect_test "delayed-output-retrieval-delayed-exit-1" =
       Sc_sys.Process.join proc;
     ]
   end;
-  [%expect{| "a" |}]
+  [%expect{|
+    "a"
+    SIGNALED(-7)
+    |}]
+;;
+
+let%expect_test "ko-join-strictly-after-stdout-consumption" =
+  run begin
+    let stdout_lines, new_stdout_line = Lwt_stream.create () in
+    let* proc =
+      bg "exit 0"
+        ~stdout:(`Grab (Push_lines new_stdout_line))
+        ~on_success:Lwt.return
+        ~on_error:(fun _ -> Lwt_fmt.printf "errored@.")
+    in
+    let x = ref 0 in
+    let* () = Lwt_stream.iter (fun _ -> incr x) stdout_lines in
+    let* () = Sc_sys.Process.join proc in
+    Lwt_fmt.printf "%d@." !x
+  end;
+  [%expect{| Test timed out |}]
 ;;
