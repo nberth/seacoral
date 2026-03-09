9ea2998d81bc18833809e69d029a7e33946e8cf5
Steven de Oliveira <steven.de-oliveira@ocamlpro.com>
2026-03-09 17:24:40 +0100

    Fixing symbolic links

--- test/cram/heavy/cbmc/ok-math-timeout.t/math.c
+++ test/cram/heavy/cbmc/ok-math-timeout.t/math.c
@@ -1,9 +1,1 @@
-#include <math.h>
-
-double obfuscated_one(float x) {
-  double c = cos(x);
-  double s = sin(x);
-  double res = c * c + s * s; 
-  if (res != 1.) return 0.;
-  return res;
-}
+../../common-files/math/math.c
